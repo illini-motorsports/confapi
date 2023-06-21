60ab0793b163c487a842ad50cc0989ba699ddb79
Collin Meyer <collinmmeyer@gmail.com>
2023-06-20 22:14:15 -0400

    fixed setup

--- setup.py
+++ setup.py
@@ -3,18 +3,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'fdplib',
-    url = 'https://github.com/illini-motorsports/fdplib',
-    version = 2.7,
+    name = 'confapi',
+    url = 'https://github.com/illini-motorsports/confapi',
+    version = 0.1,
     author = 'cmmeyer1800',
     author_email = 'collinmmeyer@gmail.com',
-    python_requires = ">=3.6",
+    python_requires = ">=3.10",
     long_description = open('README.md').read(),
     long_description_content_type='text/markdown',
     packages = (find_packages(exclude='tests')),
     install_requires=[
-        "tqdm>=4.0",
-        "numpy>=1.0",
-        "matplotlib>=3.5"
+        "requests>=2.31.0",
+        "PyYAML>=6.0"
     ]
 )
