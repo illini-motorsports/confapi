551f13a0915bb8a0d1fb784e6644916b244bfeea
Collin Meyer <collinmmeyer@gmail.com>
2023-06-23 20:23:10 -0400

    Updated documentation

--- docs/examples/get_all_users.py
+++ docs/examples/get_all_users.py
@@ -1,1 +1,8 @@
-# TODO : Create this
+from confclient import ConfClient
+
+cc = ConfClient("https://my.confluence.server.url.com", "myadminusername", "password")
+
+# Get the first 100 users
+users = cc.get_all_users(limit=100, start=0)
+
+print(users)
